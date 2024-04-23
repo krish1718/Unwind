b1b8920bc5616b89708482e3287d69a92eb18b90
Krish Manghani <krishmanghani@gmail.com>
2024-04-23 08:53:49 +0530

    fixed cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: ["https://unwind-ruby.vercel.app/"],
+        origin: ["https://unwind-ruby.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
